144ad730d757882f55250e5b980f6bec9982159b
Rafael Ferrero <rafael.ferrero@gmail.com>
2021-02-10 16:30:23 -0300

    login by post

---
+++ paycheck_upload.py
@@ -0,0 +1,24 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+def login(url, usuario, clave):
+    with requests.Session() as s:
+        s.headers.update({'Content-Type': 'application/x-www-form-urlencoded'})
+        s.auth = (usuario, clave)
+        r = s.post(url)
+    #import pdb; pdb.set_trace()
+
+
+def main():
+    loginDomain = "https://rockuapp.com/"
+    loginEndpoint = "login/"
+    loginUrl = loginDomain + loginEndpoint
+
+    login(loginUrl,
+        "rooftop@rocku.com",
+        "admin")
+
+
+if __name__ == "__main__":
+    main()
